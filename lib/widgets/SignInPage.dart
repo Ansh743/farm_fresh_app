1f683bed55f3a20b6e4834d6a7d2e1cfeab02091
Anshul M <anshmaske743@gmail.com>
2021-04-30 17:26:13 +0530

    Sigin page updated

--- lib/widgets/SignInPage.dart
+++ lib/widgets/SignInPage.dart
@@ -33,7 +33,76 @@ class _SignInPageState extends State<SignInPage> {
               height: MediaQuery.of(context).size.height * 0.1,
             ),
             // TODO: Make row for leading icon and TextFormFeild
-            TextFormField(),
+            TextFormField(
+              decoration: InputDecoration(
+                hintText: 'Enter Number',
+                icon: Container(
+                  // color: Colors.white,
+                  child: Image.asset('asset/icons/mobile-test.png'),
+                  height: 40,
+                  width: 30,
+                ),
+              ),
+              validator: (value) {
+                if (value == '') {
+                  return 'Enter number';
+                }
+              },
+              onChanged: (value) {
+                print(value);
+              },
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.1,
+            ),
+            // TODO: Make row for leading icon and TextFormFeild
+            TextFormField(
+              decoration: InputDecoration(
+                hintText: 'Enter OTP',
+                icon: Container(
+                  // color: Colors.white,
+                  child: Image.asset('asset/icons/otp.png'),
+                  height: 40,
+                  width: 30,
+                ),
+              ),
+              onChanged: (value) {
+                print(value);
+              },
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.025,
+            ),
+            Container(
+              padding: EdgeInsets.only(left: 195),
+              child: TextButton(
+                onPressed: () {
+                  print('Resending OTP');
+                },
+                child: Text('Resend OTP'),
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.025,
+            ),
+            Center(
+              child: ElevatedButton(
+                child: Container(
+                  padding: EdgeInsets.fromLTRB(90, 8, 90, 8),
+                  child: Text(
+                    'Login',
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.w600,
+                      fontFamily: 'Poppins',
+                    ),
+                  ),
+                ),
+                onPressed: () {
+                  print('Data Submitted');
+                },
+              ),
+            ),
           ],
         ),
       ),
